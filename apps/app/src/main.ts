a7e4aca43db4d247ec435549f71da64efd4e2513
haurenburu <haurenburu@protonmail.com>
2022-02-22 18:15:38 -0300

    feat: Make app service to be a microservice

--- apps/app/src/main.ts
+++ apps/app/src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { Transport } from '@nestjs/microservices';
+import { Logger } from '@nestjs/common';
+
 import { AppModule } from './app.module';
 
+const logger = new Logger("app");
+
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.createMicroservice(AppModule, {
+    logger: logger,
+    transport: Transport.TCP,
+    options: {
+      port: 4000,
+    }
+  });
+
+  await app.listen();
 }
 bootstrap();
